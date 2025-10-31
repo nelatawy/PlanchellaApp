ddececd158b9f68b1aacb222bb7567f83b195ecf
MohamedSRadwan <es.mohamed.radwan2023@alexu.edu.eg>
2025-10-31 23:34:48 +0200

    designed UI of registration

--- FrontEnd/planchella-app/src/app/app.routes.ts
+++ FrontEnd/planchella-app/src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {SignIn} from './sign-in/sign-in';
+import { Registration } from './registration/registration';
+import {ForgotPassword} from './forgot-password/forgot-password';
+import {NgModule} from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/signin', pathMatch: 'full' }, // default route
+  { path: 'signin', component: SignIn },
+  { path: 'register', component: Registration },
+  { path: 'forgot-password', component: ForgotPassword },
+  { path: '**', redirectTo: '/signin' } // fallback
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
