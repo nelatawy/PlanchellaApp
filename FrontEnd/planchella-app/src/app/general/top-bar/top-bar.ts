bb1c511270b75806d97912a886cc4813dd50dc50
nelatawy <nour.atawy2015@gmail.com>
2025-11-06 18:30:11 +0200

    added top bar

---
+++ FrontEnd/planchella-app/src/app/general/top-bar/top-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.html',
+  styleUrl: './top-bar.css',
+  standalone: false
+})
+export class TopBar {
+
+}
