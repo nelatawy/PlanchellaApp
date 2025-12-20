88c7df2979c22f4dcbe4126c7e1524d2105196ea
KarimBasem14 <155369766+KarimBasem14@users.noreply.github.com>
2025-12-20 21:21:04 +0200

    small ui updates

--- FrontEnd/planchella-app/src/app/account-page/left-side/left-side.ts
+++ FrontEnd/planchella-app/src/app/account-page/left-side/left-side.ts
@@ -1,6 +1,7 @@
-import {Component, Input} from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
 import {User} from '../../models/user';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-left-side',
@@ -14,9 +15,11 @@ export class LeftSide {
     @Input()
     user: User | undefined;
 
-    // @Input()
-    // user_name: string = "Karim Mohamed Basem"
-    //
-    // @Input()
-    // user_bio: string = ""
+    private router = inject(Router);
+
+    logout() {
+      // Clear session/token and redirect to login page
+      console.log('Logout clicked');
+      this.router.navigate(['/signin']);
+    }
 }
