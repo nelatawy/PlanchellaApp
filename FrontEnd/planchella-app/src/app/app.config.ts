1d404a7b18e392519564073dbe0fcab0bc3ea48c
nelatawy <nour.atawy2015@gmail.com>
2025-12-21 03:18:17 +0200

    added smooth transitions

--- FrontEnd/planchella-app/src/app/app.config.ts
+++ FrontEnd/planchella-app/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { authInterceptor } from './services/auth-interceptor';
 
@@ -8,7 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
